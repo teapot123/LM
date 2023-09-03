509c260cc56a9e135502f310a738dd4ac00e4f8c
teapot123 <jiaxinh3@illinois.edu>
2023-09-03 13:40:01 -0500

    preprocess

--- inference/run_preprocess.sh
+++ inference/run_preprocess.sh
@@ -2,4 +2,4 @@ mkdir ../data/trivia_qa
 python process_trivia_qa.py --down_sample True --data_split validation --select_data_num 1000
 
 huggingface-cli login --token "$(< ../t.txt)"
-python process_trivia_qa.py --data_dir ../data/trivia_qa/validation_1000 --user_prompt_file ../data/prompts/verb_1s_top1.txt
+python process_trivia_qa.py --data_dir ../data/trivia_qa/validation_1000 --user_prompt_file ../data/prompts/verb_1s_top1.txt --system_prompt_file --system_prompt_file ../data/prompts/system_prompt.txt
