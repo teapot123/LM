e0fa6564600755a27c869972a15a9a18add52684
teapot123 <jiaxinh3@illinois.edu>
2023-09-04 17:51:05 -0500

    parse answer

--- eval.py
+++ eval.py
@@ -26,6 +26,9 @@ def read_res_file(filename, first_num):
             except IndexError:
                 print(f"Cannot parse confidence: {conf}")
                 continue
+            except NameError:
+                print(f"Cannot parse confidence: {conf}")
+                continue
             res_dict[question] = [answer, conf]
 
     return res_dict
