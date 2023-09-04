c2ebaa075f5dc324eee9a8c7dcf250100fe3ea50
teapot123 <jiaxinh3@illinois.edu>
2023-09-04 14:06:27 -0500

    unmatched questions

--- eval.py
+++ eval.py
@@ -53,8 +53,8 @@ def eval_word_match(res_data, gt_data, bin_num):
             conf_list[conf_index]['correct'] += 1
         else:
             pass
-            print(f'Question: {question}')
-            print(f'Incorrect answer: {res}  Ground Truth: {gt}')
+            # print(f'Question: {question}')
+            # print(f'Incorrect answer: {res}  Ground Truth: {gt}')
         total += 1
         conf_list[conf_index]['conf'] += conf
         conf_list[conf_index]['total'] += 1
