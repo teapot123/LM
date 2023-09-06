3a2b925606ef6c544d18cd41eff1bc896eec290a
teapot123 <jiaxinh3@illinois.edu>
2023-09-05 22:25:14 -0500

    eval top k answers

--- eval.py
+++ eval.py
@@ -1,6 +1,27 @@
 import argparse
 import datasets
 
+def parse_conf(conf):
+    try:
+        if ' ' in conf:
+            conf = conf.split(' ')[0]
+        if conf[-1] == '.':
+            conf = conf[:-1]
+        conf = eval(conf)
+    except SyntaxError:
+        print(f"Cannot parse confidence: {conf}")
+        return None
+    except IndexError:
+        print(f"Cannot parse confidence: {conf}")
+        return None
+    except NameError:
+        print(f"Cannot parse confidence: {conf}")
+        return None
+    except TypeError:
+        print(f"Cannot parse confidence: {conf}")
+        return None
+    return conf
+
 def read_res_file(filename, first_num):
     res_dict = {}
     with open(filename) as f:
@@ -8,31 +29,36 @@ def read_res_file(filename, first_num):
             if first_num != None and i >= first_num:
                 continue
             tmp = line.split('\t')
-            if len(tmp) != 3:
-                print(f'no valid answers: {line}')
+            answers = []
+            confs = []
+            if len(tmp) == 3:
+                question = tmp[0].strip()
+                answer = tmp[1].strip()
+                conf = tmp[2].strip()
+                conf = parse_conf(conf)
+                if conf != None:
+                    answers.append(answer)
+                    confs.append(conf)
+            else:
+                ind = 1
+                for t in tmp:
+                    if len(t.strip()) == 0:
+                        continue
+                    if f'G{ind}' in t:
+                        tok = f'G{ind}:'
+                        answer = t.split(tok)[1].strip().split('(')[0].strip()    
+                    if f'P{ind}' in t:
+                        tok = f'P{ind}:'
+                        conf = t.split(tok)[1].strip().split('(')[0].strip()
+                        conf = parse_conf(conf)
+                        if conf != None:
+                            answers.append(answer)
+                            confs.append(conf)
+                        ind += 1
+            if answers != []:
+                res_dict[question] = [answers, confs]
+            else:
                 continue
-            question = tmp[0].strip()
-            answer = tmp[1].strip()
-            conf = tmp[2].strip()
-            try:
-                if ' ' in conf:
-                    conf = conf.split(' ')[0]
-                if conf[-1] == '.':
-                    conf = conf[:-1]
-                conf = eval(conf)
-            except SyntaxError:
-                print(f"Cannot parse confidence: {conf}")
-                continue
-            except IndexError:
-                print(f"Cannot parse confidence: {conf}")
-                continue
-            except NameError:
-                print(f"Cannot parse confidence: {conf}")
-                continue
-            except TypeError:
-                print(f"Cannot parse confidence: {conf}")
-                continue
-            res_dict[question] = [answer, conf]
 
     return res_dict
 
@@ -55,22 +81,23 @@ def eval_word_match(res_data, gt_data, bin_num):
             # print(f'question not in results: {question}')
             continue
         gt = gt_data[question]
-        res, conf = res_data[question]
-        conf_index = int(conf * bin_num)
-        if conf_index == bin_num:
-            conf_index = bin_num - 1
-        # exact word match
-        gt_list = list(set([g.lower() for g in gt]))
-        if res.lower() in gt_list:
-            correct += 1
-            conf_list[conf_index]['correct'] += 1
-        else:
-            pass
-            # print(f'Question: {question}')
-            # print(f'Incorrect answer: {res}  Ground Truth: {gt}')
-        total += 1
-        conf_list[conf_index]['conf'] += conf
-        conf_list[conf_index]['total'] += 1
+        answers, confs = res_data[question]
+        for (res, conf) in zip(answers, confs):
+            conf_index = int(conf * bin_num)
+            if conf_index == bin_num:
+                conf_index = bin_num - 1
+            # exact word match
+            gt_list = list(set([g.lower() for g in gt]))
+            if res.lower() in gt_list:
+                correct += 1
+                conf_list[conf_index]['correct'] += 1
+            else:
+                pass
+                # print(f'Question: {question}')
+                # print(f'Incorrect answer: {res}  Ground Truth: {gt}')
+            total += 1
+            conf_list[conf_index]['conf'] += conf
+            conf_list[conf_index]['total'] += 1
     ece = 0
     for i, bin in enumerate(conf_list):
         if bin['total'] == 0:
--- run_eval.sh
+++ run_eval.sh
@@ -1,5 +1,25 @@
-TEMP=1.0
+TEMP=0.5
 
 python eval.py --dataset trivia_qa \
-    --res_file generated/trivia_qa/gen_val_1000_verb_1s_top1_${TEMP}.txt \
-    --bin 20
+    --res_file generated/trivia_qa/gen_val_1000_verb_1s_top2_${TEMP}.txt \
+    --bin 10
+
+
+# DIRECTORY="generated/trivia_qa"
+
+# # Loop through all .txt files in the directory
+# for FILE in $DIRECTORY/*.txt; do
+#     # Extract the filename without the path and extension
+#     FILENAME=$(basename -- "$FILE")
+#     BASENAME="${FILENAME%.*}"
+
+#     echo ${FILENAME}
+
+#     # Use the file name in your script
+#     python eval.py --dataset trivia_qa \
+#         --res_file "$DIRECTORY/$BASENAME.txt" \
+#         --bin 10
+# done
+
+
+
